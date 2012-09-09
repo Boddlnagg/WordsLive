6e1bffc85adae96360b7b280c30d8b1e13bc7c39
Kai Patrick Reisert <kpreisert@googlemail.com>
2012-09-09 17:46:07 +0200

    Fixed Impress presentation (find correct window handles)

--- Words.Slideshow.Impress.Bridge/ImpressPresentation.cs
+++ Words.Slideshow.Impress.Bridge/ImpressPresentation.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Media.Imaging;
 using unoidl.com.sun.star.awt;
 using unoidl.com.sun.star.beans;
+using unoidl.com.sun.star.container;
 using unoidl.com.sun.star.drawing;
 using unoidl.com.sun.star.frame;
 using unoidl.com.sun.star.lang;
@@ -18,16 +20,22 @@ namespace Words.Slideshow.Impress.Bridge
 	public class ImpressPresentation : SlideshowPresentationBase
 	{
 		#region P/Invokes
-		[DllImport("user32.dll", SetLastError = true)]
+		[DllImport("user32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
 		static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
 		[DllImport("user32.dll")]
 		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-		/*[DllImport("user32.dll", SetLastError = true)]
+		[DllImport("user32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
 		static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
 
-		[DllImport("user32.dll", SetLastError = true)]
+		[DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+		public static extern int GetWindowTextLength(HandleRef hWnd);
+
+		[DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+		public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
+
+		/*[DllImport("user32.dll", SetLastError = true)]
 		private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 		
 		public static IntPtr SetClassLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
@@ -65,8 +73,9 @@ namespace Words.Slideshow.Impress.Bridge
 		XModel document;
 		XComponent component;
 		SlideShowListener listener = new SlideShowListener();
-		IntPtr hWnd;
+		IntPtr presentationHandle, mainHandle;
 		bool presentationEnded;
+		bool isShown;
 
 		ImpressMedia media;
 
@@ -87,13 +96,14 @@ namespace Words.Slideshow.Impress.Bridge
 				XComponentLoader componentLoader = (XComponentLoader)multiServiceFactory.createInstance("com.sun.star.frame.Desktop");
 				component = componentLoader.loadComponentFromURL(CreateFileUrl(media.File), "_blank", 0, new PropertyValue[] { });
 
-
-				// Hide the window and set display
+				// Get the main window's handle and hide the window
 				document = (XModel)component;
 				XWindow window = document.getCurrentController().getFrame().getContainerWindow();
 				window.setVisible(false);
 				XSystemDependentWindowPeer xWindowPeer = (XSystemDependentWindowPeer)(window);
-				var handle = xWindowPeer.getWindowHandle(new byte[] { }, 1);
+				mainHandle = new IntPtr((int)xWindowPeer.getWindowHandle(new byte[] { }, SystemDependent.SYSTEM_WIN32).Value);
+				ShowWindow(mainHandle, 0);
+
 				presentation = (XPresentation2)((XPresentationSupplier)component).getPresentation();
 
 				CreateThumbnails();
@@ -113,21 +123,6 @@ namespace Words.Slideshow.Impress.Bridge
 
 				Start();
 				controller.gotoSlideIndex(0);
-
-				hWnd = FindWindow("SALTMPSUBFRAME", null);
-				//hWnd2 = FindWindowEx(hWnd, IntPtr.Zero, "SALOBJECT", null);
-				//hWnd3 = FindWindowEx(wnd, IntPtr.Zero, "SALOBJECTCHILD", null);
-
-				//int CS_DROPSHADOW = 0x20000;
-				//var bit = GetClassLongPtr(hWnd, -26).ToInt32() | CS_DROPSHADOW;
-				//SetClassLong(hWnd, -26, new IntPtr(GetClassLongPtr(hWnd, -26).ToInt32() ^ CS_DROPSHADOW));
-				//bit = GetClassLongPtr(hWnd, -26).ToInt32() | CS_DROPSHADOW;
-
-				// Resizing the window works (but a dropshadow remains) and moving doesn't, so we stay fullscreen
-				//MoveWindow(hWnd, 0, 0, 800, 600, true);
-
-
-				ShowWindow(hWnd, 0);
 
 				LoadPreviewProvider();
 
@@ -154,13 +149,19 @@ namespace Words.Slideshow.Impress.Bridge
 			return 0;
 		}
 
+		private string GetWindowTitle(IntPtr hWnd)
+		{
+			var handleRef = new HandleRef(this, hWnd);
+			int capacity = GetWindowTextLength(handleRef) * 2;
+			StringBuilder stringBuilder = new StringBuilder(capacity);
+			GetWindowText(handleRef, stringBuilder, stringBuilder.Capacity);
+			return stringBuilder.ToString();
+		}
+
 		private void Start()
 		{
-			// TODO (Slideshow.Impress): when restarting (after presentation has finished),
-			// the Presenter Console is shown -> find that window/frame and hide it
 			controller = null;
 			presentation.setPropertyValue("Display", new uno.Any(GetDisplayIndex()));
-			var props = ((XPropertySetInfo)presentation.getPropertySetInfo()).getProperties();
 			presentation.start();
 
 			int i = 1;
@@ -175,6 +176,39 @@ namespace Words.Slideshow.Impress.Bridge
 				throw new InvalidOperationException(); // TODO (Slideshow.Impress)
 
 			controller.addSlideShowListener(listener);
+
+			controller.gotoSlideIndex(0);
+
+			IntPtr presenterConsoleHandle;
+
+			GetWindowHandles(out presenterConsoleHandle, out presentationHandle);
+
+			//int CS_DROPSHADOW = 0x20000;
+			//var bit = GetClassLongPtr(hWnd, -26).ToInt32() | CS_DROPSHADOW;
+			//SetClassLong(hWnd, -26, new IntPtr(GetClassLongPtr(hWnd, -26).ToInt32() ^ CS_DROPSHADOW));
+			//bit = GetClassLongPtr(hWnd, -26).ToInt32() | CS_DROPSHADOW;
+
+			// Resizing the window works (but a dropshadow remains) and moving doesn't, so we stay fullscreen
+			//MoveWindow(hWnd, 0, 0, 800, 600, true);
+
+			ShowWindow(presenterConsoleHandle, 0); // hide presenter console
+
+			if(!isShown)
+				ShowWindow(presentationHandle, 0); // hide presentation window if needed
+		}
+
+		private void GetWindowHandles(out IntPtr presenterConsoleHandle, out IntPtr presentationHandle)
+		{
+			presenterConsoleHandle = FindWindow("SALTMPSUBFRAME", GetWindowTitle(mainHandle));
+			IntPtr parent = presenterConsoleHandle;
+			IntPtr child = IntPtr.Zero;
+
+			while (child == IntPtr.Zero)
+			{
+				parent = FindWindowEx(IntPtr.Zero, parent, "SALTMPSUBFRAME", null);
+				child = FindWindowEx(parent, IntPtr.Zero, "SALOBJECT", null);
+			}
+			presentationHandle = parent;
 		}
 
 		private string CreateFileUrl(string path)
@@ -189,13 +223,14 @@ namespace Words.Slideshow.Impress.Bridge
 			for (int i = 0; i < pages.getCount(); i++)
 			{
 				var page = (XDrawPage)pages.getByIndex(i).Value;
+				var name = (page as XNamed).getName();
 				(document.getCurrentController() as XDrawView).setCurrentPage(page);
 				(document as XStorable).storeToURL(CreateFileUrl(file), new PropertyValue[] { CreateProperty("FilterName", new uno.Any("impress_png_Export")) });
 				using (StreamReader reader = new StreamReader(file))
 				{
 					var decoder = new PngBitmapDecoder(reader.BaseStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
 					decoder.Frames[0].Freeze();
-					thumbnails.Add(new SlideThumbnail { Image = decoder.Frames[0], Title = "Folie "+(i+1)});
+					thumbnails.Add(new SlideThumbnail { Image = decoder.Frames[0], Title = name + " (Folie "+(i+1)+")"});
 				}
 			}
 			File.Delete(file);
@@ -237,7 +272,8 @@ namespace Words.Slideshow.Impress.Bridge
 
 		public override void Show()
 		{
-			ShowWindow(hWnd, 1);
+			isShown = true;
+			ShowWindow(presentationHandle, 1);
 			Controller.FocusMainWindow();
 		}
 
@@ -257,7 +293,8 @@ namespace Words.Slideshow.Impress.Bridge
 
 		public override void Hide()
 		{
-			ShowWindow(hWnd, 0);
+			isShown = false;
+			ShowWindow(presentationHandle, 0);
 		}
 
 		public override IList<SlideThumbnail> Thumbnails
