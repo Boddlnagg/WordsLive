bdd1795dcd36dbe8bcc5e0e6c66e40f1b7894ffa
Kai Patrick Reisert <kpreisert@googlemail.com>
2012-09-11 17:18:31 +0200

    tabified & small fix

--- Words/MediaOrderList/IconProvider.cs
+++ Words/MediaOrderList/IconProvider.cs
@@ -5,59 +5,66 @@ using Words.Core;
 
 namespace Words.MediaOrderList
 {
-    public class IconProvider
-    {
-        private Media data;
-        private ImageSource cached;
+	public class IconProvider
+	{
+		private Media data;
+		private ImageSource cached;
 
-        public IconProvider(Media data)
-        {
-            this.data = data;
-        }
+		public IconProvider(Media data)
+		{
+			this.data = data;
+		}
 
-        public ImageSource Icon
-        {
-            get
-            {
-                if (cached == null)
-                    cached = CreateIcon();
-                return cached;
-            }
-        }
+		public ImageSource Icon
+		{
+			get
+			{
+				if (cached == null)
+					cached = CreateIcon();
+				return cached;
+			}
+		}
 
-        public void Update()
-        {
-            cached = null;
-        }
+		public void Update()
+		{
+			cached = null;
+		}
 
-        protected virtual ImageSource CreateIcon()
-        {
-            ImageSource icon;
+		protected virtual ImageSource CreateIcon()
+		{
+			ImageSource icon;
 
-            if (!String.IsNullOrEmpty(this.Data.File))
-            {
-                using (System.Drawing.Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(this.Data.File))
-                {
-                    icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
-                                sysicon.Handle,
-                                System.Windows.Int32Rect.Empty,
-                                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
-                }
-            }
-            else
-            {
-                icon = null;
-            }
+			if (!String.IsNullOrEmpty(this.Data.File))
+			{
+				try
+				{
+					using (System.Drawing.Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(this.Data.File))
+					{
+						icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+									sysicon.Handle,
+									System.Windows.Int32Rect.Empty,
+									System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+					}
+				}
+				catch
+				{
+					icon = null;
+				}
+			}
+			else
+			{
+				icon = null;
+			}
 
-            return icon;
-        }
+			return icon;
+		}
 
-        protected Media Data
-        {
-            get
-            {
-                return data;
-            }
-        }
-    }
+		protected Media Data
+		{
+			get
+			{
+				return data;
+			}
+		}
+	}
 }
