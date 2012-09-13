28c63f532e44051918171990a4ff799df4b86444
Kai Patrick Reisert <kpreisert@googlemail.com>
2012-09-13 15:14:13 +0200

    Added lock for ZipLoader (can't access multiple ZipEntries of same file at once)

--- Words/Utils/ImageLoader/Loaders/ZipLoader.cs
+++ Words/Utils/ImageLoader/Loaders/ZipLoader.cs
@@ -1,20 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Ionic.Zip;
 
 namespace Words.Utils.ImageLoader.Loaders
 {
 	internal class ZipLoader: ILoader
 	{
+		private readonly static object Lock = new Object();
+
 		public Stream Load(object source)
 		{
 			if (source is ZipEntry)
 			{
 				ZipEntry entry = source as ZipEntry;
-				MemoryStream stream = new MemoryStream();
-				entry.Extract(stream);
-				stream.Flush();
-				stream.Seek(0, SeekOrigin.Begin);
-				return stream;
+				lock(Lock)
+				{
+					MemoryStream stream = new MemoryStream();
+					entry.Extract(stream);
+					stream.Flush();
+					stream.Seek(0, SeekOrigin.Begin);
+					return stream;
+				}
 			}
 			else
 			{
