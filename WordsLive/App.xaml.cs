ad32c5f54bbedf7f11b396a6dc6ffdc224325db6
Kai Patrick Reisert <kpreisert@googlemail.com>
2012-09-19 14:54:26 +0200

    Removed managed WordsLive.Awesomium.exe and renamed AwesomiumProcess to WordsLive.Awesomium.exe in build (managed process is not supported on WinXP)

--- WordsLive/App.xaml.cs
+++ WordsLive/App.xaml.cs
@@ -11,7 +11,6 @@ namespace WordsLive
 	/*
 	 * TODO:
 	 * - PDF support via pdf.js?
-	 * - sometimes AwesomiumProcess is used instead of WordsLive.Awesomium.exe
 	 * - Shortcuts in the menu are shown as "Ctrl" instead of "Strg" in German language
 	 * - Add alerts to Presentation.Wpf to be able to display messages over any WPF presentation
 	 * - Improve audio/video support (look at DMediaPlayer) and support start/stop times
