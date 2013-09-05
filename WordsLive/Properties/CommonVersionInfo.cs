eaad6f7366d7b85abbd4f28e198568c743830d82
Kai Patrick Reisert <kpreisert@googlemail.com>
2013-09-05 12:26:32 +0200

    Updated version

--- WordsLive/Properties/CommonVersionInfo.cs
+++ WordsLive/Properties/CommonVersionInfo.cs
@@ -6,6 +6,6 @@
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyFileVersion("0.9.2")]
-[assembly: AssemblyVersion("0.9.2.0")]
+[assembly: AssemblyFileVersion("0.9.3")]
+[assembly: AssemblyVersion("0.9.3.0")]
 
