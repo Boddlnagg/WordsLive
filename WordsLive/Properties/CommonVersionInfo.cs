9c516dc634e4d7a7cc36e964de2041443913a3ec
Patrick Reisert <kpreisert@gmail.com>
2021-02-14 14:33:09 +0100

    Add missing file and release 0.9.18

--- WordsLive/Properties/CommonVersionInfo.cs
+++ WordsLive/Properties/CommonVersionInfo.cs
@@ -6,8 +6,8 @@
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyFileVersion("0.9.17")]
-[assembly: AssemblyVersion("0.9.17.0")]
+[assembly: AssemblyFileVersion("0.9.18")]
+[assembly: AssemblyVersion("0.9.18.0")]
 
 // NOTE: Don't forget to update the version in app.manifest as well!
 
