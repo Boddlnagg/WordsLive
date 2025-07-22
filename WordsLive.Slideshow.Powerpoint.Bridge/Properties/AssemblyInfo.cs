5f8e9e03684de2ccc798c110ccf70fbaec3c4ed2
Aaron Spettl <aaron@spettl.de>
2025-07-22 23:25:30 +0200

    Embed interop types for Powerpoint, fixes #38

--- WordsLive.Slideshow.Powerpoint.Bridge/Properties/AssemblyInfo.cs
+++ WordsLive.Slideshow.Powerpoint.Bridge/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+// Siehe auch Target "EmbedInteropTypes"
+[assembly: ImportedFromTypeLib("Microsoft.Office.Interop.PowerPoint")]
+
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
